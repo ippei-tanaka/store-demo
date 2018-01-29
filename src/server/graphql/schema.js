84e78abf670463a8349a9af25af6a2fb1070a508
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2018-01-29 16:17:17 -0500

    Stop using graphql file due to caching problem

--- src/server/graphql/schema.js
+++ src/server/graphql/schema.js
@@ -1,3 +1,29 @@
-import { buildSchema } from "graphql";
-import schemaString from "./schema.graphql";
-export default buildSchema(schemaString);
+import {buildSchema} from "graphql";
+
+const graphql = strings => buildSchema(strings[0]);
+
+const schema = graphql`
+    type Query {
+        product(id: ID!): Product
+        products: [Product]
+    }
+
+    type Mutation {
+        createProduct(input: ProductInput!): Product
+        updateProduct(id: ID!, input: ProductInput!): Product
+        deleteProduct(id: ID!): Product
+    }
+
+    type Product {
+        id: ID!
+        name: String!
+        price: Int!
+    }
+
+    input ProductInput {
+        name: String!
+        price: Int!
+    }
+`;
+
+export default schema;
