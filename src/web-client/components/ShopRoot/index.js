5ff1c708be408db3d5f1f403e09f9f2b6b9a6b10
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2018-02-10 19:09:26 -0500

    Update styles for shop home

--- src/web-client/components/ShopRoot/index.js
+++ src/web-client/components/ShopRoot/index.js
@@ -7,23 +7,26 @@ export default ({children}) => {
     return (
         <div>
             <header className={styles.header}>
-                <Link href="/" className={styles.logoLink}>
+                <Link href="/" className={styles.logoLink} title="Store Demo">
                     <Logo/>
                 </Link>
                 <menu className={styles.menu}>
                     <li className={styles.menuItem}>
                         <button className={styles.menuButton} title="Login">
                             <i className="fas fa-user"></i>
+                            <span className={styles.menuButtonText}>Login</span>
                         </button>
                     </li>
                     <li className={styles.menuItem}>
-                        <button className={styles.menuButton} title="Cart">
+                        <button className={styles.menuButton} title="Checkout">
                             <i className="fas fa-shopping-cart"></i>
+                            <span className={styles.menuButtonText}>Checkout</span>
                         </button>
                     </li>
                     <li className={styles.menuItem}>
-                        <Link href="/admin" className={styles.menuButton} title="Admin">
+                        <Link href="/admin" className={styles.menuButton} title="Admin Site">
                             <i className="fas fa-link"></i>
+                            <span className={styles.menuButtonText}>Admin Site</span>
                         </Link>
                     </li>
                 </menu>
