75b6c8d8c32801088b6ba475c09934792b470a30
Ippei Tanaka <tanaka.ippei.jp@gmail.com>
2018-02-15 12:24:26 -0500

    Update style of shopping site

--- src/web-client/components/ShopCart/index.js
+++ src/web-client/components/ShopCart/index.js
@@ -26,40 +26,29 @@ export default class ShopHeader extends Component {
 
         return (
             <form onSubmit={(e) => {e.preventDefault(); onOrderConfirmed();}}>
-                <div className={styles.tableContainer}>
-                    <table className={styles.table}>
-                        <thead>
-                            <tr>
-                                <td>Product Name</td>
-                                <td>Price</td>
-                                <td>Quantity</td>
-                                <td>Subtotal</td>
-                                <td>Remove</td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {order.map(({product, quantity}) => {
-                                return (
-                                    <tr key={product.id}>
-                                        <td><Link className={styles.nameLink} href={`/products/${product.id}`}>{product.name}</Link></td>
-                                        <td>${product.price}</td>
-                                        <td>{quantity}</td>
-                                        <td>${product.price * quantity}</td>
-                                        <td>
-                                            <button
-                                                className={styles.removeButton}
-                                                onClick={e => {
-                                                    e.preventDefault();
-                                                    onClickRemoveButton({productId: product.id, quantity});
-                                                }}
-                                            ><i className="fas fa-trash-alt"></i></button>
-                                        </td>
-                                    </tr>
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </div>
+                <ul className={styles.orderList}>
+                    {order.map(({product, quantity}) => {
+                        return (
+                            <li key={product.id} className={styles.orderListItem}>
+                                <dl className={styles.productSpecList}>
+                                    <dt className={styles.productSpecName}>Product Name</dt>
+                                    <dd className={styles.productSpecValue}><Link className={styles.nameLink} href={`/products/${product.id}`}>{product.name}</Link></dd>
+                                    <dt className={styles.productSpecName}>Subtotal</dt>
+                                    <dd className={styles.productSpecValue}>${product.price} x {quantity} = ${product.price * quantity}</dd>
+                                </dl>
+                                <div>
+                                    <button
+                                        className={styles.removeButton}
+                                        onClick={e => {
+                                            e.preventDefault();
+                                            onClickRemoveButton({productId: product.id, quantity});
+                                        }}
+                                    ><i className="fas fa-trash-alt"></i></button>
+                                </div>
+                            </li>
+                        );
+                    })}
+                </ul>
                 <div className={styles.totalContainer}> Total: ${order.reduce((memo, {product, quantity}) => memo + quantity * product.price, 0)}</div>
                 <button type="submit" className={styles.orderButton} disabled={order.length === 0}>Make Order</button>
             </form>
